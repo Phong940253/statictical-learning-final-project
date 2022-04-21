178302a4195d918e7832456caa64c744216b8ce2
ttphuongvi <trthphuongvi2901@gmail.com>
2022-04-22 00:36:40 +0700

    Accumulation score in StudyRecord

--- frontend/src/views/StudyRecord.js
+++ frontend/src/views/StudyRecord.js
@@ -75,89 +75,319 @@ const StudyRecord = () => {
       <Card className="bg-secondary shadow  m-xl-4">
         <CardHeader className="bg-white border-0">
           <Row className="align-items-center">
-            <Col xs="8">
+            <Col xs="4">
               <h3 className="mb-0">General chapters</h3>
             </Col>
-            <Col className="text-right" xs="4">
-              <Row>
-                <h4>Accumulation score</h4>
-              </Row>
+            <Col className="text-left" xs="8">
+              <h4>Accumulation score</h4>
             </Col>
           </Row>
         </CardHeader>
         <CardBody>
-          <Form>
-            <h6 className="heading-small text-muted mb-4">Exam content</h6>
-            <div className="pl-lg-4">
-              <Row>
-                <Col lg="6">
-                  <FormGroup>
-                    <label
-                      className="form-control-label"
-                      htmlFor="custom-radio"
-                    >
-                      Select chapter
-                    </label>
-                  </FormGroup>
-                </Col>
-                <Col lg="6">
-                  <FormGroup>
-                    <label
-                      className="form-control-label"
-                      // htmlFor="custom-radio"
-                    >
-                      Time
-                    </label>
-                    <div
-                      className="custom-control custom-radio mb-3"
-                      id="custom-radio"
-                    >
-                      <input
-                        className="custom-control-input"
-                        id="customRadio5"
-                        name="radio-time"
-                        type="radio"
-                      />
-                      <label
-                        className="custom-control-label"
-                        htmlFor="customRadio5"
-                      >
-                        45 Minutes
-                      </label>
-                    </div>
-                    <div
-                      className="custom-control custom-radio mb-3"
-                      id="custom-radio1"
-                    >
-                      <input
-                        className="custom-control-input"
-                        id="customRadio6"
-                        name="radio-time"
-                        type="radio"
-                      />
-                      <label
-                        className="custom-control-label"
-                        htmlFor="customRadio6"
-                      >
-                        60 Minutes
-                      </label>
-                    </div>
-                  </FormGroup>
-                </Col>
-              </Row>
-              <Row>
-                <Col log="12" className="d-flex column">
-                  <Button
-                    className="align-self-end"
-                    color="primary"
-                    type="button"
+          <div className="pl-lg-4">
+            <Row>
+              <Col lg="4">
+                <FormGroup>
+                  <label className="form-control-label" htmlFor="custom-radio">
+                    Select chapter
+                  </label>
+                </FormGroup>
+              </Col>
+              <Col lg="8">
+                <Row>
+                  <Col
+                    lg="6"
+                    className="custom-control custom-radio mb-3"
+                    id="custom-radio1"
                   >
-                    Start
-                  </Button>
-                </Col>
-              </Row>
-            </div>
-          </Form>
+                    <input
+                      className="custom-control-input"
+                      id="customRadio5"
+                      name="radio-time"
+                      type="radio"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio5"
+                    >
+                      Ứng dụng đạo hàm để khảo sát và vẽ đồ thị hàm số
+                    </label>
+                  </Col>
+                  <Col lg="6">
+                    <div className="progress-percentage">
+                      <span>4.9 Score</span>
+                    </div>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-info"
+                        role="progressbar"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={{ width: "60%" }}
+                      ></div>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col
+                    lg="6"
+                    className="custom-control custom-radio mb-3"
+                    id="custom-radio1"
+                  >
+                    <input
+                      className="custom-control-input"
+                      id="customRadio6"
+                      name="radio-time"
+                      type="radio"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio6"
+                    >
+                      Tính đơn điệu của hàm số
+                    </label>
+                  </Col>
+                  <Col lg="6">
+                    <div className="progress-percentage">
+                      <span>4.9 Score</span>
+                    </div>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-info"
+                        role="progressbar"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={{ width: "60%" }}
+                      ></div>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col
+                    lg="6"
+                    className="custom-control custom-radio mb-3"
+                    id="custom-radio1"
+                  >
+                    <input
+                      className="custom-control-input"
+                      id="customRadio7"
+                      name="radio-time"
+                      type="radio"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio7"
+                    >
+                      Giá trị lớn nhất - Giá trị nhỏ nhất của hàm số
+                    </label>
+                  </Col>
+                  <Col lg="6">
+                    <div className="progress-percentage">
+                      <span>4.9 Score</span>
+                    </div>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-info"
+                        role="progressbar"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={{ width: "60%" }}
+                      ></div>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col
+                    lg="6"
+                    className="custom-control custom-radio mb-3"
+                    id="custom-radio1"
+                  >
+                    <input
+                      className="custom-control-input"
+                      id="customRadio8"
+                      name="radio-time"
+                      type="radio"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio8"
+                    >
+                      Đường tiệm cận
+                    </label>
+                  </Col>
+                  <Col lg="6">
+                    <div className="progress-percentage">
+                      <span>4.9 Score</span>
+                    </div>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-info"
+                        role="progressbar"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={{ width: "60%" }}
+                      ></div>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col
+                    lg="6"
+                    className="custom-control custom-radio mb-3"
+                    id="custom-radio1"
+                  >
+                    <input
+                      className="custom-control-input"
+                      id="customRadio9"
+                      name="radio-time"
+                      type="radio"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio9"
+                    >
+                      Tương giao đồ thị
+                    </label>
+                  </Col>
+                  <Col lg="6">
+                    <div className="progress-percentage">
+                      <span>4.9 Score</span>
+                    </div>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-info"
+                        role="progressbar"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={{ width: "60%" }}
+                      ></div>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col
+                    lg="6"
+                    className="custom-control custom-radio mb-3"
+                    id="custom-radio1"
+                  >
+                    <input
+                      className="custom-control-input"
+                      id="customRadio10"
+                      name="radio-time"
+                      type="radio"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio10"
+                    >
+                      Tiếp tuyến của đồ thị hàm số
+                    </label>
+                  </Col>
+                  <Col lg="6">
+                    <div className="progress-percentage">
+                      <span>4.9 Score</span>
+                    </div>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-info"
+                        role="progressbar"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={{ width: "60%" }}
+                      ></div>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col
+                    lg="6"
+                    className="custom-control custom-radio mb-3"
+                    id="custom-radio1"
+                  >
+                    <input
+                      className="custom-control-input"
+                      id="customRadio11"
+                      name="radio-time"
+                      type="radio"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio11"
+                    >
+                      Sự biến thiên và đồ thị hàm số
+                    </label>
+                  </Col>
+                  <Col lg="6">
+                    <div className="progress-percentage">
+                      <span>4.9 Score</span>
+                    </div>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-info"
+                        role="progressbar"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={{ width: "60%" }}
+                      ></div>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col
+                    lg="6"
+                    className="custom-control custom-radio mb-3"
+                    id="custom-radio1"
+                  >
+                    <input
+                      className="custom-control-input"
+                      id="customRadio12"
+                      name="radio-time"
+                      type="radio"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio12"
+                    >
+                      Cực trị của hàm số
+                    </label>
+                  </Col>
+                  <Col lg="6">
+                    <div className="progress-percentage">
+                      <span>4.9 Score</span>
+                    </div>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-info"
+                        role="progressbar"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={{ width: "60%" }}
+                      ></div>
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+            <Row>
+              <Col log="12" className="d-flex column">
+                <Button
+                  className="align-self-end"
+                  color="primary"
+                  type="button"
+                >
+                  Start
+                </Button>
+              </Col>
+            </Row>
+          </div>
         </CardBody>
       </Card>
     </>
